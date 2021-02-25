5bb35f6058faf6e4c8ea45da4608a98f0c219eca
Saisyam <saisyam@saisyam.com>
2021-02-25 16:26:18 +0530

    First UI template

---
+++ ui/src/components/app.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import { Router } from 'preact-router';
+
+import Dashboard from './dashboard';
+
+const App = () => (
+	<div id="app">
+		<Dashboard />
+	</div>
+)
+
+export default App;
